f3c9af96eb256c7eb4f33cbaef43a53bf6812d3b
German Archila <106097211+4rchila@users.noreply.github.com>
2024-10-23 17:52:57 -0600

    subi el resto de imagenes Agregar archivos de proyecto.

---
+++ GestionDeBiblioteca/Biblioteca.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeBiblioteca
+{
+    public class Libros 
+    {
+        public string Titulo {  get; set; }
+        public string Autor { get; set; }
+        public string ISBN { get; set; }
+        public string Genero { get; set; }
+        public bool Disponibilidad { get; set; }
+        public int TotalPrestamo { get; set; }
+    
+
+        public Libros(string titulo, string autor, string isbn, string genero, bool disponibilidad, int totalPrestamos)
+        {
+            this.Titulo = titulo;
+            this.Autor = autor;
+            this.ISBN = isbn;
+            this.Genero = genero;
+            this.Disponibilidad = disponibilidad;
+            this.TotalPrestamo = totalPrestamos;
+        }
+    }
+}
---
+++ GestionDeBiblioteca/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeBiblioteca
+{
+    public class Usuario
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Rol { get; set; }
+
+        public Usuario(string id, string name, string rol)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Rol = rol;
+        }
+    }
+}
